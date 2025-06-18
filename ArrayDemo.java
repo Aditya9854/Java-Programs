694ff64b0715df820a957d952bb6ac76635a4ceb
Aditya Patil <85945553+Aditya9854@users.noreply.github.com>
2025-06-19 00:00:31 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,23 @@
+
+class ArrayDemo
+{
+
+    public static void main(String A[])
+    {
+       int Arr[] = {10,20,30,40};       //static
+      // int Brr[5] = {10,20,30,40};    //NA
+      
+      int Crr[] = new int[4];          //dynamic
+
+      Crr[0] = 10;
+      Crr[1] = 20;
+      Crr[2] = 30;
+      Crr[3] = 40;
+
+      System.out.println("Length of Crr is : "+Crr.length);     
+
+      String str = "Marvellous";
+      System.out.println("Length of str is : "+str.length());
+
+    }
+}
