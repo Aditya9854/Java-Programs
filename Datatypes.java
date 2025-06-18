60e7fa700f41a2887fc774b20780ee3756352064
Aditya Patil <85945553+Aditya9854@users.noreply.github.com>
2025-06-18 23:59:24 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,19 @@
+class Datatypes
+{
+
+    public static void main(String A[])
+    {
+        boolean bValue = true;
+        byte byValue = 51;
+        char ch = 'A';
+        short sValue = 6789;
+        int i = 21;
+        float fValue = 90.99f;
+        double dValue = 90.998877;
+        long lValue = 98743321;
+        
+        System.out.println("value of char is : "+ch);
+        System.out.println("value of long is : "+lValue);
+    }
+
+}
